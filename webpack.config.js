3e7002384d132eb22992b7fb514fffdef6c7acbf
Алиса Белоус <alices1308@gmail.com>
2023-02-28 15:59:30 +0200

    Update webpack configuration to optimize images

--- webpack.config.js
+++ webpack.config.js
@@ -58,6 +58,28 @@ module.exports = (_env, options) => {
                     },
                 },
                 {
+                    loader: 'image-webpack-loader',
+                    options: {
+                      mozjpeg: {
+                        progressive: true,
+                        quality: 65
+                      },
+                      optipng: {
+                        enabled: false,
+                      },
+                      pngquant: {
+                        quality: [0.3, 0.7],
+                        speed: 4
+                      },
+                      gifsicle: {
+                        interlaced: false,
+                      },
+                      webp: {
+                        quality: 75
+                      }
+                    }
+                },
+                {
                     test: /\.(html)$/,
                     use: {
                         loader: 'html-loader',
